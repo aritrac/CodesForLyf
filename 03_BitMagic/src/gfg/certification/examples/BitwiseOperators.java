50fac0de5718db2954de9824c9d683ee7dfef9ab
aritchat <aritrachatterjee2007@gmail.com>
2020-09-27 19:45:56 +0530

    Adding problems

---
+++ 03_BitMagic/src/gfg/certification/examples/BitwiseOperators.java
@@ -0,0 +1,14 @@
+package gfg.certification.examples;
+
+/**
+ * In this example we will see the three bitwise operators 'and' 'or' 'xor'
+ */
+
+public class BitwiseOperators {
+    public static void main(String[] args) {
+        int x = 3, y =6;
+        System.out.println("x & y = " + (x & y));
+        System.out.println("x & y = " + (x | y));
+        System.out.println("x & y = " + (x ^ y));
+    }
+}
